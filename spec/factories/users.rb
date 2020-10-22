d7aed859fa9d21d584cfd7d924cbda1eeaa05c62
hikinikuman <hakoniwabantyo@gmail.com>
2020-10-22 12:28:52 +0900

    Rubocop使用

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,14 +1,14 @@
 FactoryBot.define do
   factory :user do
-    nickname              {"naki"}
-    email                 {Faker::Internet.free_email}
+    nickname              { 'naki' }
+    email                 { Faker::Internet.free_email }
     password = Faker::Internet.password(min_length: 6)
-    password              {password}
-    password_confirmation {password}
-    family_name           {"中薗"}
-    first_name            {"樹"}
-    family_name_kana      {"ナカゾノ"}
-    first_name_kana       {"タツキ"}
-    birth_day             {"1998-11-17"}
+    password              { password }
+    password_confirmation { password }
+    family_name           { '中薗' }
+    first_name            { '樹' }
+    family_name_kana      { 'ナカゾノ' }
+    first_name_kana       { 'タツキ' }
+    birth_day             { '1998-11-17' }
   end
 end
