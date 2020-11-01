4eb3d268678f6c33e18c3946f8a2617510fcf0d2
hikinikuman <hakoniwabantyo@gmail.com>
2020-11-01 13:16:40 +0900

    prefecture_id 変更

--- spec/factories/purchase_addresses.rb
+++ spec/factories/purchase_addresses.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :purchase_address do
     post_code     { '123-4567' }
-    prefecture_id { '2' }
+    prefecture_id { 2 }
     municipality  { '東京都' }
     address       { '1-1' }
     building_name { '東京ハイツ' }
